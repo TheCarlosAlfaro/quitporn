35f8d523dc8faaca0c5d71ad9aef0e4035c4c542
devbysalas <rgkalin@gmail.com>
2020-01-08 17:40:27 -0800

    Tries to fix firebase

--- src/components/base.js
+++ src/components/base.js
@@ -2,9 +2,9 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  apiKey: 'AIzaSyAv7p-dMj0n2cXI9z9wBFdqUyEK3Iam6PY',
+  authDomain: 'quitporn-9f93b.firebaseapp.com',
+  databaseURL: 'https://quitporn-9f93b.firebaseio.com',
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
