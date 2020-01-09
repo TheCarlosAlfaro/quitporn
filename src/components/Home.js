cffe7b37e5fcc595a64c8a8809e053e2cac237b9
devbysalas <rgkalin@gmail.com>
2020-01-08 16:53:33 -0800

    Testing deployment

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
       <h1>Home</h1>
-      <button onClick={() => app.auth().signOut()}>Sign out</button>
+      <button onClick={() => app.auth().signOut()}>Sign outt</button>
     </>
   );
 };
