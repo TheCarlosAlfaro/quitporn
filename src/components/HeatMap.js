2366d5a483742852c0ec88b2af2ee479bc8beec9
devbysalas <rgkalin@gmail.com>
2020-01-09 16:38:43 -0800

    Adds heatmap with 3 different color codes

--- src/components/HeatMap.js
+++ src/components/HeatMap.js
@@ -1,26 +1,143 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+import { Container, Row, Button } from 'react-bootstrap';
+import Chart from 'react-apexcharts';
 
 class HeatMap extends React.Component {
-  handleClick = () => {
-    console.log('Button clicked');
-  };
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      series: [
+        // 0 = unrecorded
+        // 2 = Yes
+        // 3 = NO
+        {
+          name: 'Saturday',
+          data: [0, 1, 1, 2, 0, 0, 0, 3, 0, 0, 2, 0, 1, 1, 0]
+        },
+        {
+          name: 'Friday',
+          data: [1, 2, 2, 2, 0, 2, 2, 3, 3, 3, 1, 3, 1, 3, 0, 3]
+        },
+        {
+          name: 'Thursday',
+          data: [0, 1, 1, 3, 3, 0, 0, 1, 3, 0, 3, 1]
+        },
+        {
+          name: 'Wednesday',
+          data: [3, 3, 3, 1, 1, 0, 0, 1, 3, 0, 3, 3, 1, 0, 3, 1, 2, 2, 2, 2]
+        },
+        {
+          name: 'Tuesday',
+          data: [1, 3, 1, 3, 1, 2, 2, 2, 2, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3]
+        },
+        {
+          name: 'Monday',
+          data: [1, 1, 3, 3, 1, 2, 0, 2, 0, 0, 1, 1, 0, 1, 0, 0, 1]
+        },
+        {
+          name: 'Sunday',
+          data: [
+            3,
+            2,
+            1,
+            1,
+            3,
+            0,
+            2,
+            1,
+            1,
+            1,
+            0,
+            2,
+            0,
+            3,
+            0,
+            1,
+            1,
+            1,
+            0,
+            0,
+            0,
+            1,
+            0,
+            1,
+            0,
+            1,
+            0,
+            0,
+            0,
+            1,
+            0
+          ]
+        }
+      ],
+      options: {
+        chart: {
+          height: 350,
+          type: 'heatmap'
+        },
+        stroke: {
+          width: 0
+        },
+        plotOptions: {
+          heatmap: {
+            radius: 50,
+            enableShades: false,
+            colorScale: {
+              ranges: [
+                {
+                  from: 2,
+                  to: 2,
+                  color: '#DC3545',
+                  name: 'YES'
+                },
+                {
+                  from: 3,
+                  to: 100,
+                  color: '#27A745',
+                  name: 'NO'
+                },
+                {
+                  from: 0,
+                  to: 1,
+                  color: '#EEEEEE',
+                  name: 'Unrecorded'
+                }
+              ]
+            }
+          }
+        },
+        dataLabels: {
+          enabled: false,
+          style: {
+            colors: ['#fff']
+          }
+        },
+        xaxis: {
+          type: 'category'
+        },
+        title: {
+          text: 'The big picture'
+        }
+      }
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <div className="welcome-message">
-            <p>Welcome</p>
-            <p>to</p>
-          </div>
-          <h1 className="logo">Heat Map</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-          <Button variant="primary" onClick={this.handleClick}>
-            Primary
-          </Button>
-        </header>
-      </div>
+      <>
+        <Container>
+          <Row className="justify-content-md-center my-5">
+            <h1>How are you doing?</h1>
+          </Row>
+          <Chart
+            options={this.state.options}
+            series={this.state.series}
+            type="heatmap"
+            height={350}
+          />
+        </Container>
+      </>
     );
   }
 }
