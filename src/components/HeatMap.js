09469a6f463228a59e8907ade24a47798c9b6cb5
devbysalas <rgkalin@gmail.com>
2020-01-09 17:42:16 -0800

    Starting HeatMap from scratch

--- src/components/HeatMap.js
+++ src/components/HeatMap.js
@@ -1,143 +1,20 @@
 import React from 'react';
-import { Container, Row, Button } from 'react-bootstrap';
-import Chart from 'react-apexcharts';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import app from './base';
 
 class HeatMap extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      series: [
-        // 0 = unrecorded
-        // 2 = Yes
-        // 3 = NO
-        {
-          name: 'Saturday',
-          data: [0, 1, 1, 2, 0, 0, 0, 3, 0, 0, 2, 0, 1, 1, 0]
-        },
-        {
-          name: 'Friday',
-          data: [1, 2, 2, 2, 0, 2, 2, 3, 3, 3, 1, 3, 1, 3, 0, 3]
-        },
-        {
-          name: 'Thursday',
-          data: [0, 1, 1, 3, 3, 0, 0, 1, 3, 0, 3, 1]
-        },
-        {
-          name: 'Wednesday',
-          data: [3, 3, 3, 1, 1, 0, 0, 1, 3, 0, 3, 3, 1, 0, 3, 1, 2, 2, 2, 2]
-        },
-        {
-          name: 'Tuesday',
-          data: [1, 3, 1, 3, 1, 2, 2, 2, 2, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3]
-        },
-        {
-          name: 'Monday',
-          data: [1, 1, 3, 3, 1, 2, 0, 2, 0, 0, 1, 1, 0, 1, 0, 0, 1]
-        },
-        {
-          name: 'Sunday',
-          data: [
-            3,
-            2,
-            1,
-            1,
-            3,
-            0,
-            2,
-            1,
-            1,
-            1,
-            0,
-            2,
-            0,
-            3,
-            0,
-            1,
-            1,
-            1,
-            0,
-            0,
-            0,
-            1,
-            0,
-            1,
-            0,
-            1,
-            0,
-            0,
-            0,
-            1,
-            0
-          ]
-        }
-      ],
-      options: {
-        chart: {
-          height: 350,
-          type: 'heatmap'
-        },
-        stroke: {
-          width: 0
-        },
-        plotOptions: {
-          heatmap: {
-            radius: 50,
-            enableShades: false,
-            colorScale: {
-              ranges: [
-                {
-                  from: 2,
-                  to: 2,
-                  color: '#DC3545',
-                  name: 'YES'
-                },
-                {
-                  from: 3,
-                  to: 100,
-                  color: '#27A745',
-                  name: 'NO'
-                },
-                {
-                  from: 0,
-                  to: 1,
-                  color: '#EEEEEE',
-                  name: 'Unrecorded'
-                }
-              ]
-            }
-          }
-        },
-        dataLabels: {
-          enabled: false,
-          style: {
-            colors: ['#fff']
-          }
-        },
-        xaxis: {
-          type: 'category'
-        },
-        title: {
-          text: 'The big picture'
-        }
-      }
-    };
-  }
   render() {
     return (
-      <>
-        <Container>
-          <Row className="justify-content-md-center my-5">
-            <h1>How are you doing?</h1>
-          </Row>
-          <Chart
-            options={this.state.options}
-            series={this.state.series}
-            type="heatmap"
-            height={350}
-          />
-        </Container>
-      </>
+      <Container>
+        <Row className="my-5">
+          <h1>Coming soon...</h1>
+        </Row>
+        <Row>
+          <Col>
+            <Button onClick={() => app.auth().signOut()}>Sign out</Button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
